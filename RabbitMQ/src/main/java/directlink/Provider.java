803bad81b6b8afe6f2197eacceb0a74a857d2cfc
Chenchufeng <437121948@qq.com>
2021-03-23 23:54:37 +0800

    rabbiemq provider connect to the queue and send message

---
+++ RabbitMQ/src/main/java/directlink/Provider.java
@@ -0,0 +1,10 @@
+package directlink;
+
+/**
+ * @Author: chufeng Chen
+ * @Description: 第一种：直连模型
+ * @Date:Create：in 2021/3/23 22:39
+ */
+public class Sender {
+    
+}
---
+++ RabbitMQ/src/test/java/Test_Provider.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+/**
+  * @Author: chufeng Chen
+  * @Description:
+  * @Date:Create：in  2021/3/23 22:43
+  */public class Test_Provider {
+}
