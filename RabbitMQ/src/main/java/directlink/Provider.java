bb179e5f044641e8610cd89e874fbe6f27750f06
Chenchufeng <437121948@qq.com>
2021-03-24 23:27:16 +0800

    实现生产者和消费者监听消息队列

--- RabbitMQ/src/main/java/directlink/Provider.java
+++ RabbitMQ/src/main/java/directlink/Provider.java
@@ -1,10 +1,34 @@
 package directlink;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import util.ConnectionUtil;
 
 /**
  * @Author: chufeng Chen
  * @Description: 第一种：直连模型
  * @Date:Create：in 2021/3/23 22:39
  */
-public class Sender {
-    
+
+public class Provider {
+
+    private final static String QUEUE_NAME = "q_test_01";
+
+    public static void main(String[] argv) throws Exception {
+        // 获取到连接以及mq通道
+        Connection connection = ConnectionUtil.getConnection();
+        // 从连接中创建通道
+        Channel channel = connection.createChannel();
+
+        // 声明（创建）队列
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        // 消息内容
+        String message = "Hello World! send success!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+        //关闭通道和连接
+        channel.close();
+        connection.close();
+    }
 }
+
--- RabbitMQ/src/test/java/Test_Provider.java
+++ RabbitMQ/src/test/java/Test_Provider.java
@@ -1,7 +1,54 @@
-package PACKAGE_NAME;
 /**
-  * @Author: chufeng Chen
-  * @Description:
-  * @Date:Create：in  2021/3/23 22:43
-  */public class Test_Provider {
+ * @Author: chufeng Chen
+ * @Description:
+ * @Date:Create：in 2021/3/23 22:43
+ */
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Test_Provider {
+    @Test
+    //生产消息
+    public void testSendMessage(){
+        //创建连接mq的连接工厂
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        //设置连接rabbitmq的主机
+        connectionFactory.setHost("47.119.120.170");
+        //设置端口号
+        connectionFactory.setPort(5672);
+        //设置连接哪个虚拟主机
+        connectionFactory.setVirtualHost("Kevin");
+        //设置访问虚拟主机的用户名密码
+        connectionFactory.setUsername("Kevin");
+        connectionFactory.setPassword("123");
+        //获取连接对象
+        try {
+            Connection connection = connectionFactory.newConnection();
+            //获取连接的通道
+            Channel channel = connection.createChannel();
+            //通道绑定对应的消息队列
+            // 参数1：队列名称，如果队列不存在自动创建
+            //参数2：用来定义队列的特性是否要持久化，true为持久化 false为不持久化
+            //参数3:exclusive 是否独占队列 true为独占队列 false为不独占
+            //参数4：auto delete 是否在消费完成后自动删除队列 true为自动删除，false为不自动删除
+            //参数5：额外附加参数
+            channel.queueDeclare("Myqueue",false,false,false,null);
+            //发布消息
+            //参数1：交换机名称 参数2：队列名称 参数3：传递消息额外设置 参数4：消息的具体内容
+            channel.basicPublish("","Myqueue",null,"hello rabbitmq".getBytes());
+            channel.close();
+            connection.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
