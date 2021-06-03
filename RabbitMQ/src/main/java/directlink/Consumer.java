05dafd551a858f1491178ce6c9f771e73b0d647e
Chenchufeng <437121948@qq.com>
2021-06-03 23:59:41 +0800

    rabbitmq

--- RabbitMQ/src/main/java/directlink/Consumer.java
+++ RabbitMQ/src/main/java/directlink/Consumer.java
@@ -18,12 +18,12 @@ public class Consumer {
         //创建会话通道,生产者和mq服务所有通信都在channel通道中完成
         Channel channel = connection.createChannel();
         // 声明队列
-        //参数：String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        //参
         /**
          * 参数明细
          * 1、queue 队列名称
          * 2、durable 是否持久化，如果持久化，mq重启后队列还在
-         * 3、exclusive 是否独占连接，队列只允许在该连接中访问，如果connection连接关闭队列则自动删除,如果将此参数设置true可用于临时队列的创建
+         * 3、exclusive 数：String queue, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments是否独占连接，队列只允许在该连接中访问，如果connection连接关闭队列则自动删除,如果将此参数设置true可用于临时队列的创建
          * 4、autoDelete 自动删除，队列不再使用时是否自动删除此队列，如果将此参数和exclusive参数设置为true就可以实现临时队列（队列不用了就自动删除）
          * 5、arguments 参数，可以设置一个队列的扩展参数，比如：可设置存活时间
          */
---
+++ RabbitMQ/src/main/java/rabbitmq/simple/Producer.java
@@ -0,0 +1,9 @@
+package rabbitmq.simple;
+
+/**
+ * @Author: chufeng Chen
+ * @Description:
+ * @Date:Create：in 2021/6/3 23:58
+ */
+public class Producer {
+}
