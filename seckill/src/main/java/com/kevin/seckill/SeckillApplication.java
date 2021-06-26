e33c013ed79e4f43e744c9bb335220a7fcf1616c
Chenchufeng <437121948@qq.com>
2021-06-26 22:12:35 +0800

    æ¶秒杀系统的项目搭建

---
+++ seckill/src/main/java/com/kevin/seckill/SeckillApplication.java
@@ -0,0 +1,15 @@
+package com.kevin.seckill;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.kevin.seckill.pojo")  //com.kevin.seckill.pojo包下面的接口类，在编译之后都会生成相应的实现类
+public class SeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillApplication.class, args);
+    }
+
+}
---
+++ spring-basic/src/main/java/com/juc/ThreadLocalTest.java
@@ -0,0 +1,7 @@
+package com.juc;
+/**
+  * @Author: chufeng Chen
+  * @Description:
+  * @Date:Create：in  2021/6/24 23:49
+  */public class ThreadLocalTest {
+}
