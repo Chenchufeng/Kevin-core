fb7fac94406570d925c20eb10c621b7f9af3388a
Chufeng Chen <437121948@qq.com>
2021-10-14 22:06:21 +0800

    stringtable的使用

---
+++ JVM/src/main/java/memory/Code_05_StringTableTest.java
@@ -0,0 +1,7 @@
+package memory;/**
+ * @Author: Kevin
+ * @Date: 2021/10/14 22:05
+ * @Description: 
+ */
+public class Code_05_StringTableTest {
+}
---
+++ JVM/src/main/java/memory/Test.java
@@ -0,0 +1,7 @@
+package memory;/**
+ * @Author: Kevin
+ * @Date: 2021/10/14 21:43
+ * @Description: 
+ */
+public class Test {
+}
